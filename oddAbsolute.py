c5dcfd011e2ff8ae725e414b5dad982af67bd124
Shawn Allen <Shawn.Allen@vu.com>
2025-03-21 12:26:18 -0500

    Added try catch for user input. Got abs value first then, double if the input number is above 21.

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,19 @@
 def calculateAbsolute():
+    # Catches bad input and gets a number to calculate absolute value from the user
+    try:
+        in_num  = int(input("Enter a number: ")) # input from user
+    except:
+        print("Bad number")
+        exit()
+
+    # Calculates the absolute value of input number - 21
+    absolue_difference = abs(in_num - 21)
+
+    # If the input number is greater than 21, double it
+    if in_num > 21:
+        absolue_difference *= 2
     
-    # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    print(f"Result: {absolue_difference}")
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
